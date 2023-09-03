c7ae35640576a9095c420bba58b5fce356a8d85a
Mark-O-S <>
2023-09-03 10:13:03 +0000

    added checkout views and template

--- fit_frontier/settings.py
+++ fit_frontier/settings.py
@@ -55,6 +55,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +72,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'fit_frontier.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -83,8 +89,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins' : [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
