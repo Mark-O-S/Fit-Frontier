0a2567157d93d90af6e681daf1c417c066fdacce
Mark-O-S <mark96basilan@yahoo.com>
2023-09-04 16:34:31 +0000

    Set debug settings

--- fit_frontier/settings.py
+++ fit_frontier/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-mark-o-s-fit-frontier-swjq4ir562.us2.codeanyapp.com',
