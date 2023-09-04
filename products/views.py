6abd7236963aaabba4efaedfd5f4a6ba8c4a9333
Mark-O-S <mark96basilan@yahoo.com>
2023-09-04 10:14:58 +0000

    Fixing the image field part 1 for product admin

--- products/views.py
+++ products/views.py
@@ -56,7 +56,7 @@ def all_products(request):
         'current_sorting': current_sorting,
     }
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/store.html', context)
 
 
 def product_detail(request, product_id):
