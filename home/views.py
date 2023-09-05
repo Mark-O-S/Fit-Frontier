bb75eafebe0dedc7b455cc576e35dc47c57ee2d6
Mark-O-S <mark96basilan@yahoo.com>
2023-09-05 18:53:25 +0000

    Added About page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def about(request):
+    """A view to return the about page"""
+
+    return render(request, 'home/about.html')
