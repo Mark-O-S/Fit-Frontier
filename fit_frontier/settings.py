913a0571c1147e251d4b27bd78d4618f4c2bf9f7
Mark-O-S <mark96basilan@yahoo.com>
2023-09-05 12:33:49 +0000

    Fix typo for 'django.core.mail.backends.smtp.EmailBackend'

--- fit_frontier/settings.py
+++ fit_frontier/settings.py
@@ -233,7 +233,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'fitfrontierhq@email.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = 'True' 
     EMAIL_PORT = 587 # Port for TLS
     EMAIL_HOST = 'smtp.gmail.com' # SMTP server for Gmail
