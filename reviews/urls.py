664a605c960175b1bfd6716eed540cf0c79db0c5
Mark-O-S <mark96basilan@yahoo.com>
2023-11-04 18:14:27 +0000

    Add review product page

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:product_id>', views.product_review, name='product_review'),
+    path('', views.list_all_reviews, name='list_all_reviews'),
+    path('add_product_review/<int:product_id>/', views.add_product_review, name='add_product_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,30 @@
-from .models import Product, Category
+from .models import Review, Product
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 
-def product_review(request, product_id):
+def add_product_review(request, product_id):
     """ A view to show an individual product review """
+    print("Success")
 
     product = get_object_or_404(Product, pk=product_id)
+    product_review = Review.objects.filter(product=product)
 
     context = {
-        'product': product,
+        'product_review': product_review,
     }
 
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'reviews/product_review.html', context)
+
+
+def list_all_reviews(request):
+    """ A view to show an individual product review """
+    print("Success")
+
+    product = get_object_or_404(Product, pk=product_id)
+    product_review = Review.objects.filter(product__in=product)
+
+    context = {
+        'product_review': product_review,
+    }
+
+    return render(request, 'reviews/product_review.html', context)
