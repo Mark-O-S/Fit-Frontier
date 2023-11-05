6382a6a9c038a87fb6607468e29c4f5b0e95b4cc
Mark-O-S <mark96basilan@yahoo.com>
2023-11-05 13:03:42 +0000

    Add redirects for unregistered users for reviews

--- reviews/views.py
+++ reviews/views.py
@@ -17,7 +17,7 @@ def add_review(request, product_id):
 
     user_product_review = (
         [product_review for product_review in product.reviews.all()
-            if request.user == review.product_review]
+            if request.user == product_review.reviewed_by]
     )
     user_reviewed_product = len(user_product_review) > 0
 
@@ -43,10 +43,16 @@ def add_review(request, product_id):
             messages.success(request, "Your review has been created!")
             return render(request, 'products/product_detail.html', context)
     
+    else:
+        if user_reviewed_product:
+            messages.warning(request, "You have already created a review "
+                                      "for this product. You can edit your "
+                                      "review instead!")
+            return redirect(reverse('product_detail', args=[product_id]))
+
     return render(request, 'products/product_detail.html', context)
 
 
-# This is not correctly updating the review
 @login_required
 def edit_product_review(request, product_id):
     """ A view to update an individual product review """
@@ -78,24 +84,69 @@ def edit_product_review(request, product_id):
             user_product_review[0].rating = rating
             user_product_review[0].comment = content
             user_product_review[0].headline = headline
+            user_product_review[0].save()
 
             messages.success(request, "Your review has been updated!")
             return render(request, 'products/product_detail.html', context)
-    
+
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
+def delete_product_review(request, product_id):
+    """ A view to delete an individual product review """
+    if not request.user.is_authenticated:
+        messages.warning(request, "Only logged in users can delete a review")
+        return render(request, 'home/index.html')
+
+    product = get_object_or_404(Product, pk=product_id)
+    product_reviews = Review.objects.filter(product=product)
+
+    user_product_review = (
+        [product_review for product_review in product.reviews.all()
+            if request.user == product_review.reviewed_by]
+    )
+
+    context = {
+        'product': product,
+        'user_reviewed_product': False,
+        'product_reviews': product_reviews
+    }
+
+    if request.method == 'POST':
+        user_product_review[0].delete()
+
+        messages.success(request, "Your review has been deleted!")
+        return render(request, 'products/product_detail.html', context)
+
+    return render(request, 'products/product_detail.html', context)
+
+
+@login_required
 def show_review_product(request, product_id):
     """ A view to add an individual product review """
     product = get_object_or_404(Product, pk=product_id)
+
+    user_product_review = (
+        [product_review for product_review in product.reviews.all()
+            if request.user == product_review.reviewed_by]
+    )
+    user_reviewed_product = len(user_product_review) > 0
 
     context = {
         'product': product,
     }
 
+    if user_reviewed_product:
+        messages.warning(request, "You have already created a review "
+                                    "for this product. You can edit your "
+                                    "review instead!")
+        return redirect(reverse('product_detail', args=[product_id]))
+
     return render(request, 'reviews/add_product_review.html', context)
 
 
+@login_required
 def show_edit_product_review(request, product_id):
     """ A view to edit an individual product review """
     product = get_object_or_404(Product, pk=product_id)
@@ -107,12 +158,23 @@ def show_edit_product_review(request, product_id):
     return render(request, 'reviews/edit_product_review.html', context)
 
 
-def delete_product_review(request, product_id):
-    """ A view to add an individual product review """
+@login_required
+def show_delete_product_review(request, product_id):
+    """ A view to delete an individual product review """
     product = get_object_or_404(Product, pk=product_id)
+
+    user_product_review = (
+        [product_review for product_review in product.reviews.all()
+            if request.user == product_review.reviewed_by]
+    )
+    user_reviewed_product = len(user_product_review) > 0
 
     context = {
         'product': product,
     }
 
-    return render(request, 'reviews/add_product_review.html', context)
+    if not user_reviewed_product:
+        messages.warning(request, "You have no review to delete for this product. You can create a new review!")
+        return redirect(reverse('product_detail', args=[product_id]))
+
+    return render(request, 'reviews/delete_product_review.html', context)
