f1ed0373dff78ad719f00ecc72c3eab737bcda40
Mark-O-S <mark96basilan@yahoo.com>
2023-11-05 15:04:47 +0000

    Add test field to reviews object model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Review
 
-admin.site.register(Review)
+# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'headline',
+        'rating',
+        'comment',
+        'reviewed_by',
+        'reviewed_at',
+    )
+
+    ordering = ('reviewed_at',)
+
+admin.site.register(Review, ReviewAdmin)
