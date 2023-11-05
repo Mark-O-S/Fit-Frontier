f7d69f9ce539269a0fde234c3d1f1200527262e4
Mark-O-S <mark96basilan@yahoo.com>
2023-11-05 13:00:41 +0000

    Add delete review url in reviews app

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('add_review/<int:product_id>/', views.add_review, name='add_review'),
     path('edit_review/<int:product_id>/', views.edit_product_review, name='edit_product_review'),
-    path('delete_review/<int:product_id>/', views.delete_product_review, name='delete_review'),
+    path('delete_review/<int:product_id>/', views.show_delete_product_review, name='show_delete_product_review'),
+    path('delete_product_review/<int:product_id>/', views.delete_product_review, name='delete_product_review'),
     path('show_review_product/<int:product_id>/', views.show_review_product, name='show_review_product'),
     path('show_edit_product_review/<int:product_id>/', views.show_edit_product_review, name='show_edit_product_review'),
 ]
