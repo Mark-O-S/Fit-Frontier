55f2bfdd3378166ef647b9326b8a8a7592826977
Mark-O-S <mark96basilan@yahoo.com>
2023-11-05 14:47:21 +0000

    Simplify admin review file for debugging

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,18 +1,4 @@
 from django.contrib import admin
 from .models import Review
 
-# Register your models here.
-
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = (
-        'product',
-        'rating',
-        'comment',
-        'reviewed_by',
-        'reviewed_at',
-        'headline',
-        )
-
-    ordering = ('reviewed_at',)
-
-admin.site.register(Review, ReviewAdmin)
+admin.site.register(Review)
