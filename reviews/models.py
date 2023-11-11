6333a2ee62e213477be50417bdd971361bf15038
Mark-O-S <mark96basilan@yahoo.com>
2023-11-11 18:37:47 +0000

    Add null and blank attributes to review model fields

--- reviews/models.py
+++ reviews/models.py
@@ -5,12 +5,12 @@ from products.models import Product
 # Create your models here.
 class Review(models.Model):
     product = models.ForeignKey(
-        Product, related_name='reviews', on_delete=models.CASCADE
+        Product, related_name='reviews', on_delete=models.CASCADE, null=False, blank=False
         )
-    rating = models.IntegerField(default=5)
-    comment = models.TextField()
+    rating = models.IntegerField(default=5, null=False, blank=False)
+    comment = models.TextField(null=False, blank=True)
     reviewed_by = models.ForeignKey(
-        User, related_name='reviews', on_delete=models.CASCADE
+        User, related_name='reviews', on_delete=models.CASCADE, null=False, blank=False
         )
-    reviewed_at = models.DateTimeField(auto_now_add=True)
-    headline = models.TextField()
+    reviewed_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    headline = models.TextField(null=False, blank=True)
